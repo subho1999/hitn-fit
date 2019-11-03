ac6e45527015499d3d7c73d0cb33652e6fd0f184
subho1999 <ghosh.subho.1999@gmail.com>
2019-11-03 21:53:22 +0530

    Third commit

--- index.js
+++ index.js
@@ -8,4 +8,25 @@ let db = mongoose.connection;
 db.once("open", () => console.log("Connected to MongoDB"));
 db.on("error", err => console.log(err));
 
-const User = require("./models/User");
+let userSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true
+  }
+});
+
+const User = new mongoose.model("user", userSchema);
+
+app.use(express.static(path.join(__dirname, "public")));
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => console.log(`Server started on PORT ${PORT}`));
