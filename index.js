5144ea238f497402374a788d20ccb84d5a3faf39
subho1999 <ghosh.subho.1999@gmail.com>
2019-11-03 23:25:25 +0530

    Pre final commit

--- index.js
+++ index.js
@@ -8,22 +8,60 @@ let db = mongoose.connection;
 db.once("open", () => console.log("Connected to MongoDB"));
 db.on("error", err => console.log(err));
 
-let userSchema = mongoose.Schema({
-  name: {
-    type: String,
-    required: true
-  },
-  password: {
-    type: String,
-    required: true
-  },
-  email: {
-    type: String,
-    required: true
-  }
+const userSchema = mongoose.Schema({
+  name: String,
+  password: String,
+  email: String
 });
 
-const User = new mongoose.model("user", userSchema);
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+const User = new mongoose.model("users", userSchema);
+
+app.post("/signup", (req, res) => {
+  signupDetails = {
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.psw
+  };
+  User.create(signupDetails, (err, response) => {
+    if (err) {
+      res.redirect("/notFound");
+    } else {
+      res.redirect("/success");
+    }
+  });
+});
+
+app.get("/notFound", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "notfound.html"));
+});
+
+app.get("/success", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "login.html"));
+});
+
+app.post("/login", (req, res) => {
+  loginDetails = {
+    email: req.body.email,
+    password: req.body.psw
+  };
+  User.find({ email: loginDetails.email, password: loginDetails.password }, (err, response) => {
+    if (err) throw err;
+    else {
+      if (Object.entries(response).length == 0) {
+        res.redirect("/notFound");
+      } else {
+        res.redirect("/successLogin");
+      }
+    }
+  });
+});
+
+app.get("/successLogin", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "success.html"));
+});
 
 app.use(express.static(path.join(__dirname, "public")));
 
