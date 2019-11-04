2b442e46919939e366994de91efd9e793bc494b1
subho1999 <ghosh.subho.1999@gmail.com>
2019-11-04 22:43:15 +0530

    Pre-Final Commit

--- index.js
+++ index.js
@@ -3,7 +3,10 @@ const mongoose = require("mongoose");
 const path = require("path");
 const app = express();
 
-mongoose.connect("mongodb://localhost/userFitDB");
+mongoose.connect("mongodb://localhost/userFitDB", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
 let db = mongoose.connection;
 db.once("open", () => console.log("Connected to MongoDB"));
 db.on("error", err => console.log(err));
